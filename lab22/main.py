5d0a99193aa7b0811b12aa5b8ceb4971ae3f9043
PioGol <piotr.golec@outlook.com>
2018-04-15 15:59:00 +0200

    0,3

--- lab22/main.py
+++ lab22/main.py
@@ -1,3 +1,18 @@
-print("hej")
-a = 20
-print(a+a)
+def dodawanie(a, b):
+
+    return a+b
+
+def odejmowanie(a, b):
+
+    return a-b
+
+def mnozenie(a, b):
+
+    return a*b
+
+a = 2
+b = 4
+
+print(dodawanie(a, b))
+print(odejmowanie(a, b))
+print(mnozenie(a, b))
